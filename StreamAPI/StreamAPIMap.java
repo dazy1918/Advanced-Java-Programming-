1bb65ba69d630faab8ffe3042046099625cb3b99
Dazy Baliyan <baliyandazy22@gmail.com>
2026-03-17 10:08:59 +0530

    Add StreamAPIMap.java for filtering and mapping integers
    Implement Stream API to filter and square even numbers from a list.

---
+++ StreamAPI/StreamAPIMap.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class Stream3 {
+    public static void main(String[] args) {
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(45);
+        l3.add(87);
+        l3.add(76);
+        l3.add(98);
+
+        Stream<Integer> s3 = l3.stream();
+
+        List<Integer> l4 = s3
+        .filter(x -> x % 2 == 0)
+        .map(x->x*x)
+        .collect(Collectors.toList());
+
+        System.out.println(l4);
+    }
+}
