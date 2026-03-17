8b595f9f0639f49aa6d3f899dd8f8f265e6205e9
Dazy Baliyan <baliyandazy22@gmail.com>
2026-03-17 10:01:50 +0530

    Implement switch expression for day categorization

---
+++ SwitchExpression/SwitchProgram1.java
@@ -0,0 +1,11 @@
+public class SwitchExpression {
+    public static void main(String[] args) {
+        String day = "monday";
+        String d = switch (day) {
+            case "monday", "friday", "thursday", "tuesday", "wednesday" -> "weekdays";
+            case "sunday", "saturday" -> "weekend";
+            default -> "invalid";
+        };
+        System.out.println(d);
+    }
+}
