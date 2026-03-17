a7394119bfd8e6cba1cadbac2ec3bfc3cfc664f2
Dazy Baliyan <baliyandazy22@gmail.com>
2026-03-17 10:03:23 +0530

    Add switch expression for arithmetic operations
    Implement a switch expression for basic arithmetic operations.

---
+++ SwitchExpression/SwitchProgram3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SwitchExpression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int b = 5;
+        System.out.println("Enter an operation (+, -, *, /): ");
+        char ch = sc.next().charAt(0);
+        int result = switch (ch) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> a / b;
+            default -> {
+                System.out.println("Invalid operation entered.");
+                yield -1;
+            }
+        };
+
+        System.out.println("Result: " + result);
+
+        sc.close();
+    }
+}
