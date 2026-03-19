640e12b4bd5f39404e9e8a28aeb142a0f09c4d44
Dazy Baliyan <baliyandazy22@gmail.com>
2026-03-19 21:55:01 +0530

    Add practice4.java for sorting integers
    Implement a Java program to sort and print a list of integers.

---
+++ PracticeQuestions/practice4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class practice4 {
+    public static void main(String[] args) {
+        List<Integer> n = new ArrayList<>();
+        n.add(70);
+        n.add(22);
+        n.add(2);
+        Collections.sort(n);
+        System.out.println(n + " ");
+
+    }
+}
