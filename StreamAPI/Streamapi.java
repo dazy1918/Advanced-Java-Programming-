f585ae16a33a8122441dc8483f2e8434c9ffb579
Dazy Baliyan <baliyandazy22@gmail.com>
2026-04-17 11:59:07 +0530

    Update Streamapi.java

--- StreamAPI/Streamapi.java
+++ StreamAPI/Streamapi.java
@@ -1,27 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+// Second Highest Number
+import java.util.*;
+import java.util.stream.*;
 
-public class Stream1 {
+public class Q2 {
     public static void main(String[] args) {
-        int arr[] = { 3, 4, 7, 89, 43, 80 };
-        IntStream s = Arrays.stream(arr);
-        s.forEach(x -> System.out.println(x));
+        List<Integer> list = Arrays.asList(10,20,30,40);
 
-        // 2. Value
-        Stream<Integer> s2 = Stream.of(2, 8, 6, 9, 5, 3, 0, 4);
-        s2.forEach(x -> System.out.println(x));
+        int result = list.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
 
-        // 3. Collections
-        List<Integer> l3 = new ArrayList<>();
-        l3.add(45);
-        l3.add(87);
-        l3.add(76);
-        l3.add(98);
-
-        Stream<Integer> s3 = l3.stream();
-        s3.forEach(x -> System.out.println(x));
+        System.out.println(result);
     }
 }
+
